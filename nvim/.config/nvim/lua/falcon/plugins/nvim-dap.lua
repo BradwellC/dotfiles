aa08b9a3597957486b9c979a8da4a391c8c229fd
Chris Bradwell <chrisbradwell@live.co.uk>
2025-01-17 17:14:13 +0000

    New Nvim LSP Structure

--- nvim/.config/nvim/lua/falcon/plugins/nvim-dap.lua
+++ nvim/.config/nvim/lua/falcon/plugins/nvim-dap.lua
@@ -13,23 +13,23 @@ return {
     -- setup dap ui with default config
     dapui.setup({})
 
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
     -- Set vim motion for <Space> + d + t to toggle a breakpoint at the line where the cursor is currently on
     vim.keymap.set('n', '<leader>dt', dap.toggle_breakpoint, { desc = "[D]ebug [T]oggle Breakpoint" })
     -- Set vim motion for <Space> + d + s to start the debugger & launch debug ui
     vim.keymap.set('n', '<leader>ds', dap.continue, { desc = "[D]ebug [S]tart" })
-    -- Set vim motion for <Space> + d + c to close debugger ui 
+    -- Set vim motion for <Space> + d + c to close debugger ui
     vim.keymap.set('n', '<leader>dc', dapui.close, { desc = "[D]ebug [C]lose" })
   end
 }
